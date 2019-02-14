93178570f84983447c36e2e6fabd3a30d17f7073
Wei9023 <vv880923@gmail.com>
2019-02-14 12:25:56 -0800

    feature two done

--- README.md
+++ README.md
@@ -5,6 +5,17 @@ Estimate of time needed to complete: 1hour
 
 Start time:9:20
 
-Finish time:
+Finish time: 10:45
 
-Actual time needed to complete:
+Actual time needed to complete: 1hour25min
+
+Number and name of feature: 1
+
+Estimate of time needed to complete: 1hour
+
+Start time:11:00
+
+Finish time: 
+
+Actual time needed to complete: 
+
--- js/app.js
+++ js/app.js
@@ -18,10 +18,28 @@ Photo.prototype.render = function () {
     photoClone.html(photoHtml);
     photoClone.find('h2').text(this.title);
     photoClone.find('img').attr('src', this.image_url);
-    photoClone.find('p').text(this.description);
+    photoClone.find('img').attr('alt', this.keyword + this.description);
+    // photoClone.find('p').text(this.description);
     photoClone.removeClass('clone');
     photoClone.attr('class', this.keyword);
 }
+
+Photo.prototype.selectPhoto = function () {
+    // alert(1);
+    $('#photo-selector').append($("<option></option>").attr('value', this.keyword).text(this.keyword));
+    let optionClone= $('option[class="clone"]');
+    let optionHtml = $('#photo-selector').html();
+
+    
+    optionClone.html=(optionHtml);
+    // console.log((optionClone.find('option')));
+    // console.log(typeof(optionClone.find('option')));
+    // optionClone.find('option').text(this.keyword);
+    // optionClone.find('option').attr('value', this.keyword);
+    // optionClone.removeClass('clone');
+    // optionClone.attr('id', this.keyword);
+}
+
 
 Photo.readJson = () => {
     $.get('data/page-1.json', 'json')
@@ -36,6 +54,18 @@ Photo.readJson = () => {
 
 Photo.loadPhoto = () => {
     Photo.allPhotoes.forEach(photo => photo.render())
+    Photo.allPhotoes.forEach(photo => photo.selectPhoto())
 }
+
+$('select[id="photo-selector"]').on('change', function() {
+    let $selection = $(this).val();
+    Photo.allPhotoes.forEach( photo => {
+        if(photo.keyword === $selection){
+            $(`div[class="${$selection}"]`).show();
+        }else{
+            $(`div[class="${photo.keyword}"]`).hide();
+        }        
+    });
+  })
 
 $(() => Photo.readJson());
